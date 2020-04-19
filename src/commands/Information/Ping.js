d765364d806f6588a6004643ce29a3977a4dfc6e
Axel Greavette <vultureavatar@gmail.com>
2020-04-19 14:35:26 -0400

    poing

---
+++ src/commands/Information/Ping.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    description: "Return the bots response times.",
+    category: "Information",
+    async execute(message, args, client) {
+        const sent = await message.channel.send("Preforming calculations...");
+    
+        const embed = new MessageEmbed()
+            .setColor("2f3136")
+            .setTitle("Ping!")
+            .setDescription(`Gateway: **${client.ws.ping.toFixed(2)}**\nMessage: **${sent.createdTimestamp - message.createdTimestamp}ms**`)
+
+        return sent.edit(embed);
+    }
+};
