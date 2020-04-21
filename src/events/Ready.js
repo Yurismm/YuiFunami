b7f0fbeb3fedf8c644dbf06601e4abddcb2825ce
MendtheMiner <31546304+MendtheMiner@users.noreply.github.com>
2020-04-21 17:45:11 +0100

    OCD

--- src/events/Ready.js
+++ src/events/Ready.js
@@ -3,11 +3,11 @@ const { version, dependencies } = require("../../package.json");
 
 module.exports = async (client) => {
     client.logger.info(`${magenta(client.user.username)} is online`);
-    client.logger.info(`Prefix set to ${magenta(client.prefixes.global)}`);
+    client.logger.info(`Prefix set to: ${magenta(client.prefixes.global)}`);
     client.logger.info(`${magenta(client.commands.size)} commands loaded`);
-    client.logger.info(`Program Version: ${blue("v" + version)}`);
-    client.logger.info(`Node Version: ${blue(process.version)}`);
-    client.logger.info(`Discord.js Version ${blue(dependencies["discord.js"].replace("^", "v"))}`);
+    client.logger.info(`Program version: ${blue("v" + version)}`);
+    client.logger.info(`Node version: ${blue(process.version)}`);
+    client.logger.info(`Discord.js version: ${blue(dependencies["discord.js"].replace("^", "v"))}`);
 
     if (client.debug) client.logger.info(grey("Started in DEBUG MODE"));
 
