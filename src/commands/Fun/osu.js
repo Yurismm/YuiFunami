8723d257e7b7f0a289c3df946e2edffca3e8a603
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2020-04-21 10:57:40 +0100

    osu command

---
+++ src/commands/Fun/osu.js
@@ -0,0 +1,16 @@
+const {MessageEmbed} = require('discord.js')
+
+module.exports = {
+    name:"osu",
+    description: "Returns infomation about your osu name",
+    category:"Fun",
+    async execute(message,client){
+        const args = message.content.split(" ").slice(1);
+        const osuname = args.join(" ");
+        const embed = new MessageEmbed()
+        .setTitle("osu! Profile")
+        .setImage("https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname="+osuname+"&pp=1&countryrank&flagshadow&flagstroke&opaqueavatar&avatarrounding=5&onlineindicator=undefined&xpbar&xpbarhex&random")
+        message.channel.send(embed);
+      }
+      
+  }
