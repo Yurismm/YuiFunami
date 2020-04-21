b5a6004f640f43f1c7727c9c295a633c57f4a5c4
Dries1234 <driesolbrechts@gmail.com>
2020-04-21 17:20:11 +0200

    Changed to use findMember instead

--- src/commands/Fun/challenger.js
+++ src/commands/Fun/challenger.js
@@ -8,25 +8,24 @@ module.exports = {
     description: "Sends a smash announcement.",
     category: "Fun",
     preventDefaultError: true,
-    async execute(message) {
+    async execute(message,args,client) {
         try {
-            const avatar = message.mentions.users.first()? await loadImage(message.mentions.users.first().displayAvatarURL({format: 'jpg'})): await loadImage(message.member.user.displayAvatarURL({format: 'jpg'}))
-            const text = message.mentions.members.first() ? message.mentions.members.first().displayName : message.member.displayName;
+            const mention = client.findMember(message,args[0],true)
+            const avatar = await loadImage(mention.user.displayAvatarURL({format: 'jpg'}))
+            const text = mention.displayName
             const base = await loadImage(join(__dirname, "..", "..", "..", "assets", "image", "bin", "challenger.jpg"));
             const canvas = createCanvas(base.width, base.height);
             const ctx = canvas.getContext("2d");
             ctx.drawImage(base, 0, 0);
             ctx.font = "50px Smash";
             ctx.fillStyle = "#ffffff";
-            radians = (-4.0 * Math.PI / 180);
+            radians = (-3 * Math.PI / 180);
             ctx.rotate(radians);
-            ctx.fillText(text, 500, 180);
-            ctx.rotate(-radians)
+            ctx.fillText(text, 500, 170);
+            ctx.rotate(-radians);
             radians = (-10 * Math.PI / 180);
-            ctx.rotate(radians)
-        
-
-            ctx.drawImage(avatar,20,220,270,270)
+            ctx.rotate(radians);
+            ctx.drawImage(avatar,20,220,270,270);
 
 
             return await message.channel.send({files: [{attachment: canvas.toBuffer(), name: "challenger.jpg"}]});
--- src/commands/Fun/missing.js
+++ src/commands/Fun/missing.js
@@ -7,29 +7,23 @@ module.exports = {
     description: "Person is missing.",
     category: "Fun",
     preventDefaultError: true,
-    async execute(message) {
-        let who = "";
-        if (message.mentions.members.first()) {
-            who = message.mentions.members.first();
-        } else {
-            who = message.member;
-        }
-        try {
+    async execute(message,args,client) {
+        try{
+            const mention = client.findMember(message,args[0],true)
+            const avatar = await loadImage(mention.user.displayAvatarURL({format: 'jpg'}))
             const base = await loadImage(join(__dirname,"..","..","..","assets","image","bin","missing.jpg"));
             const canvas = createCanvas(base.width, base.height);
             const ctx = canvas.getContext("2d");
             ctx.drawImage(base, 0, 0);
             ctx.font = "28px sans-serif";
             ctx.fillStyle = "#000000";
-            ctx.fillText(shortenText(ctx, who.displayName, 171), 73, 45);
+            ctx.fillText(shortenText(ctx, mention.displayName, 171), 73, 45);
 
             ctx.beginPath();
             ctx.arc(86, 296, 21, 0, Math.PI * 2, true);
             ctx.closePath();
             ctx.clip();
-            const avatar = await loadImage(
-                who.user.displayAvatarURL({ format: "jpg" })
-            );
+            
             ctx.drawImage(avatar, 62, 275, 50, 50);
 
             return await message.channel.send({
