649a7a469bbc5512d031c68588350d9656c688b1
Dries1234 <driesolbrechts@gmail.com>
2020-04-21 12:48:58 +0200

    fun commands

---
+++ src/commands/Fun/batslap.js
@@ -0,0 +1,44 @@
+const {createCanvas, loadImage, registerFont} = require("canvas");
+const { join } = require("path");
+const { shortenText } = require("../../util/Canvas");
+registerFont(join(__dirname, "..", "..", "..", "assets", "font", "Minecraftia.ttf"), { family: "Minecraftia" });
+
+module.exports = {
+    name: "batslap",
+    aliases: ["slap"],
+    description: "slap someone",
+    args: true,
+    usage: '<@mention>',
+    category: "Fun",
+    preventDefaultError: true,
+    async execute(message, args) {
+        try {
+            const base = await loadImage(join(__dirname, "..", "..", "..", "assets", "image", "bin", "batslap.jpg"));
+            const canvas = createCanvas(base.width, base.height);
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(base, 0, 0);
+            
+            ctx.beginPath();
+            ctx.arc(364, 384, 140, 0, Math.PI * 2, true);
+            ctx.arc(720, 226, 140, 0, Math.PI * 2, true);
+	        ctx.closePath();
+            ctx.clip();
+
+            const mentionAvatar = await loadImage(message.mentions.users.first().displayAvatarURL({ format: 'jpg' }));
+
+            ctx.drawImage(mentionAvatar,220,240,300,300)
+
+            const avatar = await loadImage(message.member.user.displayAvatarURL({ format: 'jpg' }));
+
+            ctx.drawImage(avatar,580,85,300,300)
+
+           
+            return await message.channel.send({files: [{attachment: canvas.toBuffer(), name: "batslap.jpg"}]});
+        }catch(error){
+            throw error.message;
+        }
+    },
+    async error(message, args, client, error) {
+        return message.channel.send(`\`\`\`${error}\`\`\``);
+    }
+};
