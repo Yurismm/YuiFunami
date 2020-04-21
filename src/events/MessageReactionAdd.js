0e79d2455657110d61dbb1cf09f4f5b4f38331a8
Dries1234 <32861260+Dries1234@users.noreply.github.com>
2020-04-21 15:00:27 +0200

    Update MessageReactionAdd.js

--- src/events/MessageReactionAdd.js
+++ src/events/MessageReactionAdd.js
@@ -3,7 +3,7 @@ module.exports = async(client,reaction,user) => {
     if(reaction.message.author.id !== client.user.id) return;
     const content = reaction.message.content.replace(/```/g,"").replace(/\n/g,"").replace("!", "").replace("#", "").replace("-", "");
     let usertag = user.tag;
-    if(reaction.message.edits.length >= 1){
+    if(reaction.message.edits.length > 1){
         usertag = "";
     }
     switch(reaction.emoji.name){
