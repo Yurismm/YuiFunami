a5d4fd35e7d112b1762a8ac64eca83962f773372
Dries1234 <driesolbrechts@gmail.com>
2020-05-01 17:34:02 +0200

    no embeds starring and also no going over max field size

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -7,13 +7,19 @@ module.exports = class {
   async execute(reaction, user) {
     if (await this.client.starboards.get(reaction.message.guild.id)) {
       if (reaction.emoji.name === "⭐") {
+        if(reaction.message.embeds[0]){
+          return;
+        }
         const starboard = await this.client.starboards.get(reaction.message.guild.id);
         if (reaction.count == starboard.stars) {
           const starchannel = reaction.message.guild.channels.cache.get(starboard.channelId);
+          let content = reaction.message.content;
+          if(content.length > 1024) content = this.client.util.shorten(reaction.message.content,1024);
+          
           const embed = new MessageEmbed()
           .addField("Author", reaction.message.author,true)
           .addField("Channel", reaction.message.channel,true)
-          .addField("Message", reaction.message.content,false)
+          .addField("Message", content,false)
           .setTimestamp(reaction.message.createdAt.toISOString())
           .addField("Jump",`[jump here](${reaction.message.url})`)
           .setThumbnail(reaction.message.author.displayAvatarURL())
