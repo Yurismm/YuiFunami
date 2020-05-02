68132c0b325c0a8668bec53bf6040bbc6d41b2a3
Mariobob <regmibatshal@gmail.com>
2020-05-02 12:36:42 -0400

    api thing

---
+++ src/commands/Fun/Abandon.js
@@ -0,0 +1,20 @@
+const Command = require("../../struct/Command");
+const { MessageAttachment } = require("discord.js");
+const superagent = require("superagent");
+
+module.exports = class Abandon extends Command {
+  constructor(client) {
+    super(client, {
+      name: "abandon",
+      description: "Abandon someone.",
+    });
+  }
+  async run(message, [text]) {
+    const res = await superagent
+      .get("localhost/api/abandon")
+      .query({ text: text });
+    const attachment = new MessageAttachment(res.body, "abandon.png");
+    message.channel.startTyping();
+    await message.channel.send(attachment);
+  }
+};
