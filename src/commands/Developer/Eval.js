92a997ed5c44ce04deee3df7c82f025ae1545ee4
Mariobob <regmibatshal@gmail.com>
2020-05-20 12:11:16 -0400

    better eval

--- src/commands/Developer/Eval.js
+++ src/commands/Developer/Eval.js
@@ -1,42 +1,81 @@
+const { MessageEmbed } = require("discord.js");
+const util = require("util");
 const Command = require("../../struct/Command");
 
 module.exports = class Eval extends Command {
-    constructor(client) {
-        super(client, {
-            name: "eval",
-            description: "Evaluates arbitrary Javascript.",
-            usage: "eval <expression>",
-            aliases: ["ev"],
-            permLevel: "Bot Admin",
-        });
+  constructor(client) {
+    super(client, {
+      name: "eval",
+      description: "Evaluates arbitrary Javascript.",
+      usage: "eval <expression>",
+      aliases: ["ev"],
+      permLevel: "Bot Admin",
+    });
+  }
+
+  async run(message, args, level) {
+    var code = args.join(" ");
+    if (!code) return message.channel.send(`I can't eval nothing.`);
+    if (
+      code.toLowerCase() === "this.client.token" ||
+      code.toLowerCase() === "process"
+    )
+      return message.channel.send("no");
+    if (code.toLowerCase().includes("process.exit"))
+      return message.channel.send("reboot instead");
+    try {
+      var evaled = eval(code);
+
+      if (typeof evaled !== "string") evaled = util.inspect(evaled);
+      const checkCharLength = clean(evaled);
+      if (code === clean(evaled))
+        return message.channel.send("The input was the same as the output.");
+      const MAX_CHARS = 3 + 2 + checkCharLength.length + 3;
+      if (MAX_CHARS > 1023) {
+        message.channel.send(
+          "Output exceeded 1024 characters. Sending as a file.",
+          {
+            files: [
+              { attachment: Buffer.from(checkCharLength), name: "output.txt" },
+            ],
+          }
+        );
+      }
+      const embed = new MessageEmbed()
+        .setColor(0x36393f)
+        .addField(":inbox_tray: Input: ", `\`\`\`${code}\`\`\``)
+        .addField(
+          ":outbox_tray: Output: ",
+          `\`\`\`js\n${clean(evaled)}\n\`\`\``
+        );
+      message.channel.send(embed);
+    } catch (err) {
+      if (err.message.length > 1023) {
+        message.channel.send(
+          "Output exceeded 1024 characters. Sending as a file.",
+          {
+            files: [
+              { attachment: Buffer.from(err.message), name: "output.txt" },
+            ],
+          }
+        );
+      }
+      const embed = new MessageEmbed()
+        .setColor(0x36393f)
+        .addField(":inbox_tray: Input: ", `\`\`\`${code}\`\`\``)
+        .addField(
+          ":outbox_tray: Output: ",
+          `\`\`\`${clean(err.message)}\`\`\``
+        );
+      message.channel.send(embed);
     }
 
-    async run(message, args, level) {
-        const code = args.join(" ");
-       
-        try {
-            const evaled = await eval(`(async () => { ${code} })()`);
-            const clean = await this.client.clean(evaled);
-            const MAX_CHARS = 3+2+ clean.length + 3;
-            if (MAX_CHARS > 2000) {
-                return message.channel.send(
-                    "Output exceeded 2000 characters. Sending as a file.",
-                    {
-                        files: [
-                            {
-                                attachment: Buffer.from(clean),
-                                name: "output.txt",
-                            },
-                        ],
-                    }
-                );
-            }
-            message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
-        } catch (err) {
-            message.channel.send(
-                `\`ERROR\` \`\`\`xl\n${err}\n\`\`\``
-            );
-        }
+    function clean(text) {
+      if (typeof text === "string")
+        return text
+          .replace(/`/g, "`" + String.fromCharCode(8203))
+          .replace(/@/g, "@" + String.fromCharCode(8203));
+      else return text;
     }
+  }
 };
-
