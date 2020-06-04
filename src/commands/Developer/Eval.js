7d9e4aac484dd62c24d00e059b3a05a347c8c4e5
Dries1234 <32861260+Dries1234@users.noreply.github.com>
2020-06-04 10:25:41 +0200

    Some more eval changes to fix it properly

--- src/commands/Developer/Eval.js
+++ src/commands/Developer/Eval.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 const util = require("util");
 const Command = require("../../struct/Command");
-
+const colors = require('../../util/Colors')
 module.exports = class Eval extends Command {
   constructor(client) {
     super(client, {
@@ -17,16 +17,13 @@ module.exports = class Eval extends Command {
     const code = args.join(" ");
     if (!code) return message.channel.send(`I can't eval nothing.`);
 
-    try {
       let evaled = await eval(`(async () => { ${code} })()`);
 
       if (typeof evaled !== "string") evaled = util.inspect(evaled);
-      const checkCharLength = clean(evaled);
-      if (code === clean(evaled))
-        return message.channel.send("The input was the same as the output.");
+      const checkCharLength = await this.client.clean(evaled);
       const MAX_CHARS = 3 + 2 + checkCharLength.length + 3;
       if (MAX_CHARS > 1023) {
-        message.channel.send(
+        return message.channel.send(
           "Output exceeded 1024 characters. Sending as a file.",
           {
             files: [
@@ -40,36 +37,9 @@ module.exports = class Eval extends Command {
         .addField(":inbox_tray: Input: ", `\`\`\`${code}\`\`\``)
         .addField(
           ":outbox_tray: Output: ",
-          `\`\`\`js\n${clean(evaled)}\n\`\`\``
+          `\`\`\`js\n${await this.client.clean(evaled)}\n\`\`\``
         );
       message.channel.send(embed);
-    } catch (err) {
-      if (err.message.length > 1023) {
-        message.channel.send(
-          "Output exceeded 1024 characters. Sending as a file.",
-          {
-            files: [
-              { attachment: Buffer.from(err.message), name: "output.txt" },
-            ],
-          }
-        );
-      }
-      const embed = new MessageEmbed()
-        .setColor(0x36393f)
-        .addField(":inbox_tray: Input: ", `\`\`\`${code}\`\`\``)
-        .addField(
-          ":outbox_tray: Output: ",
-          `\`\`\`${clean(err.message)}\`\`\``
-        );
-      message.channel.send(embed);
-    }
 
-    function clean(text) {
-      if (typeof text === "string")
-        return text
-          .replace(/`/g, "`" + String.fromCharCode(8203))
-          .replace(/@/g, "@" + String.fromCharCode(8203));
-      else return text;
-    }
   }
 };
